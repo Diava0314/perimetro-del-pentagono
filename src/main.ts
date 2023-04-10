27c2eba59bff3165fd1ef814d5b96de085438aca
Diava0314 <juan14diavanera@gamil.com>
2023-04-10 12:53:51 -0500

    pentagono

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+alert("Hoy vamos a calcular el perimetro de un pentagono");
+
+let lado: number = Number(prompt("asigne la longitud del lado"));
+let perimetro: number = (lado*5);
+
+alert("el perimetro es: " + perimetro);
